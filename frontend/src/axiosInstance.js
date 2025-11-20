e9e76f3a90a4a668164e1f9448fc36406b672762
Deepak_Kumar <ydeepak62076@gmail.com>
2025-11-20 19:42:45 +0530

    Bug fixed

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,19 +1,9 @@
 import axios from "axios";
-import { toast } from "react-toastify";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
-// // Global Error Handler
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     const msg = error?.response?.data?.message || "Something went wrong!";
-//     toast.error(msg);
-//     return Promise.reject(error);
-//   }
-// );
 
 export default axiosInstance;
