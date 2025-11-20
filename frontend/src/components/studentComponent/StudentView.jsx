227a9f3e3fe1da1940f56d7c8b1c2e975c0b2cc6
Deepak_Kumar <ydeepak62076@gmail.com>
2025-11-20 21:39:36 +0530

    Fix Bug

--- frontend/src/components/studentComponent/StudentView.jsx
+++ frontend/src/components/studentComponent/StudentView.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
 import { ToastContainer } from 'react-toastify'
 
 const StudentView = () => {
+    const location = useLocation()
     return (
         <div>
             <ToastContainer position='top-right' autoClose={2000} />
-            <Header />
+            {!location.pathname.includes("course-progress") ?
+                (<Header />) : null
+            }
             <Outlet />
-            <Footer />
+            {!location.pathname.includes("course-progress") ?
+                (<Footer />) : null
+            }
         </div>
     )
 }
